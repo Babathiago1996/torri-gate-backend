0c5c6a80fe26a8e76d37c42c3cc02478c23910cf
Babathiago1996 <adebisiazeez1997@gmail.com>
2025-05-26 06:20:37 -0700

    refactor :added profile pic upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app=express();
 const mongoose=require("mongoose")
 const userRouter=require("./routes/userRouter")
 const cors=require("cors")
+const cloudinary=require("cloudinary").v2
+const fileUpload=require("express-fileupload")
 const PORT=process.env.PORT || 3000;
 
 
@@ -11,6 +13,17 @@ const PORT=process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 app.use(cors())
+app.use(fileUpload({
+    useTempFiles:true,
+    limits:{fileSize:10*1024*1024}
+}))
+
+// cloudinary config
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET,
+})
 
 // routes
 app.get("/",(req,res)=>{
