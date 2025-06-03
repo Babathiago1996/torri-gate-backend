ff962310ae6486817476e7a1469bc4011cb08d60
Babathiago1996 <adebisiazeez1997@gmail.com>
2025-06-03 05:08:21 -0700

    delete option intoduce

--- routes/propertyRouters.js
+++ routes/propertyRouters.js
@@ -1,10 +1,17 @@
 const router =require("express").Router()
-const {createProperty, getLandlordsProperty,updatePropertyAvailability,getAllProperty,getSingleProperty}=require("../controller/propertyController")
+const {createProperty, getLandlordsProperty,updatePropertyAvailability,getAllProperty,getSingleProperty, deleteProperty}=require("../controller/propertyController")
 const {isLoggedIn,requiredPermissions}=require("../middleware/auth")
 
 router.post("/", isLoggedIn,requiredPermissions("landlord"), createProperty)
 router.get("/landlord", isLoggedIn, requiredPermissions("landlord"), getLandlordsProperty)
 router.patch("/landlord/:propertyId",isLoggedIn, requiredPermissions("landlord"), updatePropertyAvailability)
+router.delete(
+  "/landlord/:propertyId",
+  isLoggedIn,
+  requiredPermissions("landlord"),
+  deleteProperty
+);
+
 //tenants
 router.get("/", isLoggedIn, getAllProperty)
 router.get("/:propertyId", isLoggedIn, getSingleProperty)
